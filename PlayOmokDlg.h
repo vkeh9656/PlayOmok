2d3b5a489d6fc70b5ff792f8e7de7a6888070c1b
DESKTOP-9FFSOO7\tjdwn <tjdwns9656@naver.com>
2022-10-23 23:19:58 +0900

    초기 커밋.

---
+++ PlayOmokDlg.h
@@ -0,0 +1,33 @@
+﻿
+// PlayOmokDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CPlayOmokDlg 대화 상자
+class CPlayOmokDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CPlayOmokDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PLAYOMOK_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
